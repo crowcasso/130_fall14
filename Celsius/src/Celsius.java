8d90563bdca7fbf5c03000195bb6c2ea1e380551
crowcasso <crowcasso@gmail.com>
2014-09-01 07:58:54 -0400

    Celsius

---
+++ Celsius/src/Celsius.java
@@ -0,0 +1,14 @@
+
+public class Celsius {
+
+	public static void main(String[] args) {
+		
+		int fahr = 56;
+		
+		// compute celsius from fahrenheit
+		double celsius = (5.0 / 9.0) * (fahr - 32);
+		
+		// display the answer
+		System.out.println(fahr + " is " + celsius + " in celsius");	
+	}
+}
